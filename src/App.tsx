17c2f5e25e61d7d6cfa11c86501711b3fe95f7dc
Jason <wangsanmu5@163.com>
2024-12-13 10:18:24 +0800

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center items-center h-64">
+        <img src={viteLogo} alt="Vite Logo" className="w-32 h-32 object-contain rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out" />
+        <img src={reactLogo} alt="React Logo" className="w-32 h-32 object-contain rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="text-center p-110">
+        <h1 className="text-4xl font-bold text-blue-500">Hello TailwindCSS!</h1>
+      </div>
+      <div className='mt-8'>
+        <button onClick={() => setCount(count+1)} className='px-6 py-3 bg-blue-500 text-white font-semibold text-lg rounded-lg shadow-lg hover:bg-blue-600 hover:scale-105 transition-transform duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50'>
+          {`Count ${count}`}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
